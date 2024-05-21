c459ab74a04bf244ddcbc50dced9f2eac7b01016
Haomin Wang <898496136@qq.com>
2024-05-21 19:58:56 +0800

    路径写入txt

--- RadarThermogram/FinalMap.py
+++ RadarThermogram/FinalMap.py
@@ -1,12 +1,16 @@
 import GetPosition as gp
 import SearchRoute as sr
-import  SafetyFactor as sf
 
 if __name__ == '__main__':
     # 无人机最后的位置
     initial_position = gp.get_initial_position()
-    #print(initial_position)
+
     # 根据无人机位置修正后的路径信息
     path = sr.get_changed_path()
-    #print(path)
+    print(path)
 
+    # 将路径保存到txt文件
+    with open('./path.txt', 'w') as file:
+        for pos in path:
+            file.write(f"{pos}\n")
+    print("路径保存成功！")
